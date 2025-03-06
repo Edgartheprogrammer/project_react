1f50a0093fe1028f7880bd8db32c9e6707b18b78
Edgartheprogrammer <eo.gh@hotmail.com>
2025-03-06 21:02:05 +0100

    onclick button added

--- src/components/Botoncito.jsx
+++ src/components/Botoncito.jsx
@@ -3,7 +3,10 @@ import './Button.css'
 
 const Boton = ({text}) => {
   return (
-    <button type="button">{text}</button>
+    // <button type="button">{text}</button>
+    <button type="button" onClick={() => console.log(`Enhorabuena: ${text}`)}>
+    {text}
+  </button>
   )
 }
 
