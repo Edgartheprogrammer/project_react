8480318b9e2410a2a17ad7ee7ff7598af4e50fad
Edgartheprogrammer <eo.gh@hotmail.com>
2025-03-06 15:31:53 +0100

    created buttons and h1

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Boton from './components/Botoncito.jsx'
+
+
+function App() {
+  
+  return (
+    
+    <>
+    <div>
+      <h1>Mi primera app en React</h1>
+    </div>
+      <Boton text="Click aquí"/>
+      <Boton text="Aquí no"/>
+      <Boton text="Aquí menos"/>
+      
+
+    </>
+    
+    
+  )
+}
+
+export default App; 
+
+
+      
+      
+      
+        
+
+
---
+++ src/components/Botoncito.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Button.css'
+
+const Boton = ({text}) => {
+  return (
+    <button type="button">{text}</button>
+  )
+}
+
+export default Boton
